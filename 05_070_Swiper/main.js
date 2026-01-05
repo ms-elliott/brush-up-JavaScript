6c8cd5bead35aafa4cb47d648165dd13441c5010
ms-elliott <ms.elliott55@gmail.com>
2026-01-06 01:34:43 +0900

    Added lessons

--- 05_070_Swiper/main.js
+++ 05_070_Swiper/main.js
@@ -1,6 +1,5 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-  effect: 'fade', //'coverflow'にしてもアニメーションを変更可能
+document.addEventListener('DOMContentLoaded', function () {
+    const hero = new HeroSlider('.swiper');
+    hero.start();
 });
+
