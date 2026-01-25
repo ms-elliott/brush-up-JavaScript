3d12939696f4d27ac51941531d27843252593c12
ms-elliott <ms.elliott55@gmail.com>
2026-01-25 21:01:44 +0900

    Under construction

--- 098_SampleWebSite_practice/scripts/main.js
+++ 098_SampleWebSite_practice/scripts/main.js
@@ -1,25 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hero = new HeroSlider('.swiper');
-    hero.start();
+document.addEventListener("DOMContentLoaded", function () {
+  const hero = new HeroSlider(".swiper");
+  hero.start();
 
-    const _textAnimation = function(el, inview) {
-        if(inview) {
-            const ta = new TweenTextAnimation(el);
-            ta.animate();
-        }
+  const _textAnimation = function (el, inview) {
+    if (inview) {
+      const ta = new TweenTextAnimation(el);
+      ta.animate();
     }
+  };
 
-    const so = new ScrollObserver('.tween-animate-title', _textAnimation);
+  const so = new ScrollObserver(".tween-animate-title", _textAnimation);
 
-    const _inviewAnimation = function(el, inview) {
-        if(inview) {
-            el.classList.add('inview');
-        } else {
-            el.classList.remove('inview');
-        }
+  const _inviewAnimation = function (el, inview) {
+    if (inview) {
+      el.classList.add("inview");
+    } else {
+      el.classList.remove("inview");
     }
+  };
 
-    const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);    
+  const so2 = new ScrollObserver(".cover-slide", _inviewAnimation);
+
+  const header = document.querySelector(".header");
+  const _navAnimation = function (el, inview) {
+    if (inview) {
+      header.classList.remove("triggered");
+    } else {
+      header.classList.add("triggered");
+    }
+  };
+  
+  const so3 = new ScrollObserver(".nav-trigger", _navAnimation, { once: false });
+
+  new MobileMenu;
 });
-
-
